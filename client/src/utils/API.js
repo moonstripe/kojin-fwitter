bc803c69482bcc6cf7a3e8b1b3a92abf1d4b8578
Kojin Glick <kojinglick@gmail.com>
2020-10-31 11:06:31 -0700

    init repo, ported over app from manny's github

---
+++ client/src/utils/API.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+export default {
+  search: function() {
+    return axios.get('https://dog.ceo/api/breeds/image/random');
+  },
+  searchByBreed: function(breed) {
+    return axios.get(`https://dog.ceo/api/breed/${breed}/images`);
+  }
+};
